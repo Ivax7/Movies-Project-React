57c7ce9f1c679944b7e1742f3a29cf9469084bc7
Xavi Serrano <xserrano2001@gmail.com>
2024-09-06 18:06:25 +0200

    Event OnClick to most popular anime information

--- Aplicacion-Peliculas/src/Animes/Components/hooks/useMostPopular.jsx
+++ Aplicacion-Peliculas/src/Animes/Components/hooks/useMostPopular.jsx
@@ -3,9 +3,6 @@ import { IconThumbUp, IconBookmark } from '@tabler/icons-react';
 
 const TOP_ANIME_URL = "https://api.jikan.moe/v4/top/anime";
 
-// Función sleep para pausar la ejecución
-const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
 export function useMostPopular() {
   const [popularAnimes, setPopularAnimes] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -16,43 +13,18 @@ export function useMostPopular() {
     setError(null);
 
     try {
-      let allAnimes = [];
-      let page = 1;
-      const limit = 50;
-      const perPage = 10;
-
-      while (allAnimes.length < limit) {
-        try {
-          const response = await fetch(`${TOP_ANIME_URL}?page=${page}&limit=${perPage}`);
-          if (!response.ok) {
-            if (response.status === 429) { // Si recibimos un error 429, hacemos una pausa
-              console.warn('Rate limit exceeded, pausing...');
-              await sleep(10000); // Pausa de 10 segundos
-              continue; // Reintenta la solicitud
-            }
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          const data = await response.json();
-          allAnimes = [...allAnimes, ...data.data];
-          page++;
-          if (allAnimes.length >= limit) break;
-        } catch (error) {
-          console.error('Error fetching animes page:', error);
-          setError(error.message);
-          break;
-        }
+      const response = await fetch(TOP_ANIME_URL);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      // Ordena los animes por número de miembros en orden descendente
+      const data = await response.json();
+      let allAnimes = data.data;
+
       const sortedAnimes = allAnimes.sort((a, b) => b.members - a.members);
-
-      // Selecciona los 10 animes con más miembros
       const top10ByMembers = sortedAnimes.slice(0, 10);
-
-      // Guardar los datos en localStorage
       localStorage.setItem('popularAnimes', JSON.stringify(top10ByMembers));
 
-      // Actualizar el estado con los datos obtenidos
       setPopularAnimes(top10ByMembers);
 
     } catch (error) {
@@ -64,7 +36,6 @@ export function useMostPopular() {
   };
 
   useEffect(() => {
-    // Intentar cargar los datos desde localStorage
     const savedPopularAnimes = localStorage.getItem('popularAnimes');
 
     if (savedPopularAnimes) {
@@ -78,26 +49,38 @@ export function useMostPopular() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
+  // Función para manejar el click y redirigir a un enlace
+  const handleAnimeClick = (anime) => {
+    // Reemplaza esta URL con la estructura de enlace que desees
+    const url = `https://myanimelist.net/anime/${anime.mal_id}`;
+    
+    window.open(url, '__blank') // Redirige al usuario a la URL
+  };
+
+
   return (
     <div className="right-section">
       <article className='popular-animes'>
         <h4>Most Popular</h4>
-        {popularAnimes.map((anime, index) => (
-          <div
-            key={anime.mal_id}
-            className="popular-information"
-            style={{ backgroundImage: `url(${anime.images.jpg.large_image_url || 'default_image_url'})` }}
-          >
-            <div className="rank">{index + 1}</div>
-            <div className="title">{anime.title}</div>
-            <div className="popularity">Likes: {anime.members}</div>
-            
-            <div className="action-buttons">
-              <button className="save-button"><IconBookmark stroke={2} /></button>
-              <button className="like-button"><IconThumbUp stroke={2} /></button>
+        <div className="information">
+          {popularAnimes.map((anime, index) => (
+            <div
+              key={anime.mal_id}
+              className="popular-information"
+              style={{ backgroundImage: `url(${anime.images.jpg.large_image_url || 'default_image_url'})` }}
+              onClick={() => handleAnimeClick(anime)} // Agrega el evento onClick aquí
+            >
+              <div className="rank">{index + 1}</div>
+              <div className="title">{anime.title}</div>
+              <div className="popularity"><IconThumbUp stroke={2} /> {anime.members}</div>
+              
+              <div className="action-buttons">
+                <button className="like-button"><IconThumbUp stroke={2} /></button>
+                <button className="save-button"><IconBookmark stroke={2} /></button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </article>
     </div>
   );
